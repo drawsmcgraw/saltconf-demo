cf81f581ebc880c8b5bc55d8ad2df469fff957f8
drawsmcgraw <drew.malone@gmail.com>
2015-03-01 14:34:10 -0500

    Backing off of RS build. Will just take two runs.

--- mongo/files/configure-rs.sh
+++ mongo/files/configure-rs.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 
 mongo <<EOL
-function addHosts(){
-  {% for host,ip in salt['mine.get']('roles:*mongo*', 'network.ip_addrs', expr_form='grain').items() -%}
-  rs.add("{{host}}")
-  {% endfor -%}
-}
-
 rs.initiate()
-
-# Wait 10 seconds, then add hosts
-setTimeout(addHosts,10000)
+{% for host,ip in salt['mine.get']('roles:*mongo*', 'network.ip_addrs', expr_form='grain').items() -%}
+rs.add("{{host}}")
+{% endfor -%}
 EOL
